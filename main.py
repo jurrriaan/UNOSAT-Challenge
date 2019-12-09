73d4465b8c116f72a8c953618361b10713a6fd79
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-12-09 21:02:45 +0100

    Feat/ml wrapper class (#5)
    * feat: ML class with test train split
    
    * feat: Training method

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import app
+import src
 
 def main():
-    config = app.Config()
-    dataset = app.DataSet(config)
-    Xdata, Ydata = dataset.load_and_prepare_data('Mosul', '2015')
+    config = src.Config()
+    dataset = src.DataSet(config)
+    x_data, y_data = dataset.load_and_prepare_data('Mosul', '2015')
     i_show = 5000
-    app.visualize_sample(Xdata[i_show], Ydata[i_show])
+    src.visualize_sample(x_data[i_show], y_data[i_show])
+    ml = src.ML(config, x_data, y_data)
+    ml.train()
 
 
 if __name__ == "__main__":
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .config import Config
 from .dataset import DataSet
 from .visualization import visualize_sample
+from .ml import ML
